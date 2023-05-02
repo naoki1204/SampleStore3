b5c718c5791f4d7a7ba7ec3f9c1cd353d5aa583e
原田尚輝 <a148106.nao@gmail.com>
2023-05-02 17:01:14 +0900

    devise Userテスト追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { Faker::Lorem.characters(number: 5) }
     # 実際のemailアドレスの形を指定
     email { Faker::Internet.email }
-    encrypted_password { Faker::Lorem.characters(number: 8) }
+    password { "111111" }
+    password_confirmation { "111111" }
+    confirmed_at { Date.today }
   end
 end
