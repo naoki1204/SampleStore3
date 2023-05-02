df888739dfc4255daec849e6d3c5583ae1631389
原田尚輝 <a148106.nao@gmail.com>
2023-05-02 17:56:21 +0900

    addreddのテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :validatable
   # validates :email, presence: true
 end
