f3fbbd47ceccf36631b10a9f2b43906ba1a0c696
原田尚輝 <a148106.nao@gmail.com>
2023-06-11 16:08:45 +0900

    systemspec修正

--- spec/system/cart_spec.rb
+++ spec/system/cart_spec.rb
@@ -7,12 +7,15 @@ before do
 end
   describe '商品選択のテスト' do
 
-    it '商品選択' do
+    before '商品選択' do
       visit cart_index_path
 
       fill_in "params[0][quantity]", with: "1234"
       check "params[0][product_id]"
       click_button "注文"
+    end
+    it "注文入力画面が表示される" do
+      expect(page).to have_content "注文情報入力"
     end
 
   end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,39 +1,52 @@
 require "rails_helper"
 
 RSpec.describe Product, type: :system do
-before(:each) do
-  @product = create(:product) #ここで定義
-end
+  before(:each) do
+    @product = create(:product) #ここで定義
+  end
   describe '商品管理のテスト' do
 
-    it '商品の登録' do
-      visit new_product_path
+    context '商品の登録' do
+      before do
+        visit new_product_path
 
-      fill_in "Number", with: "1234"
-      fill_in "Name", with: "ソファー"
-      fill_in "Unit price", with: "100000"
-      fill_in "Image", with: "テスト"
-      fill_in "Description", with: "テスト"
-      fill_in "Quantity", with: "1"
-      click_button "登録する"
+        fill_in "Number", with: "1234"
+        fill_in "Name", with: "ソファー"
+        fill_in "Unit price", with: "100000"
+        fill_in "Image", with: "テスト"
+        fill_in "Description", with: "テスト"
+        fill_in "Quantity", with: "1"
+        click_button "登録する"
+      end
+      it "作成した商品が表示される" do
+        expect(page).to have_content "ソファー"
+      end
     end
 
-    it "商品の更新" do
-      visit edit_product_path(@product)
-
-      fill_in "Number", with: "1234"
-      fill_in "Name", with: "ソファー"
-      fill_in "Unit price", with: "100000"
-      fill_in "Image", with: "テスト"
-      fill_in "Description", with: "テスト"
-      fill_in "Quantity", with: "1"
-      click_button "更新する"
+    context "商品の更新" do
+      before do
+        visit edit_product_path(@product)
+        fill_in "Number", with: "1234"
+        fill_in "Name", with: "キッチンテーブル"
+        fill_in "Unit price", with: "100000"
+        fill_in "Image", with: "テスト"
+        fill_in "Description", with: "テスト"
+        fill_in "Quantity", with: "1"
+        click_button "更新する"
+      end
+      it "更新した商品が表示される" do
+        expect(page).to have_content "キッチンテーブル"
+      end
     end
+    context "商品の削除" do
+      before do
+        visit product_path(@product)
 
-    it "商品の削除" do
-      visit product_path(@product)
-
-      click_button "Destroy this product"
+        click_button "Destroy this product"
+      end
+      it "削除した商品が表示されない" do
+        expect(page).to have_no_content "キッチンテーブル"
+      end
     end
   end
 end
